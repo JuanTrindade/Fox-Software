3fbd99f99dbca8f4591ccb7c505d18bc71a04635
unknown <Administrador@DESKTOP-IVFUHS6.eaglecargo.local>
2022-06-09 14:24:39 -0300

    Add reponsive screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './Style.css';
+import React from 'react';
+import logo from './fox.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='pcontainer'>
+        <div className="Container">
+          <div className='logoContainer'>
+            <img 
+              width={100}
+              height={100}
+              className='logo'
+              src={logo}
+            />
+          </div>
+          <header>
+            <h1 className='Header'>Fox Software</h1>
+          </header>
+          <div className='textContent'>
+            <h3>
+              Acesse o sistema da Fox Engine
+            </h3>
+          </div>
+          <div className='containerInput'>
+            <p className='textUserAndPass'>Usuario</p>
+            <input 
+                type="text"
+                className="Input"
+              />
+            <div className='separation'/>
+            <p className='textUserAndPass'>senha</p> 
+            <input 
+                type="password"
+                className="Input"
+              />
+          </div>
+          <div className='containerButton'>
+            <button className='buttonTest'>Acessar</button>
+          </div>
+          <div className='centering'>
+            <a href='www.youtube.com' className='textUserAndPass'>Esqueci minha senha</a>
+          </div>
+        </div>
     </div>
   );
 }
-
 export default App;
